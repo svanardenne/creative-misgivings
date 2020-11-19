9db0b2037d71b34ffc053537d3aca2fd7186a352
Shaun van Ardenne <s.vanardenne@gmail.com>
2020-11-18 16:05:01 -0800

    put headerinfo in

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -3,7 +3,15 @@ import React from 'react';
 const MainHeader = () => {
     return(
         <div className="jumbotron">
-        
+            <div className="headline">
+                <h1>Hello!</h1>
+                <p>
+                    I'm a placeholder for more text which will describe the person behind the site.
+                </p>
+            </div>
+            <div className="down-arrow">
+                <span>^</span>
+            </div>
         </div>
     );
 }
