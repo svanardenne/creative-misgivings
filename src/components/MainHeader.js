a559ff126cf01351efeac3edc6ec49d888a26997
Shaun van Ardenne <s.vanardenne@gmail.com>
2020-11-18 16:02:23 -0800

    Changed theme and main header side

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -3,12 +3,7 @@ import React from 'react';
 const MainHeader = () => {
     return(
         <div className="jumbotron">
-            <div className="headline">
-                <h1>Hello!</h1>
-                <p>
-                    I'm a placeholder for more text which will describe the person behind the site.
-                </p>
-            </div>
+        
         </div>
     );
 }
